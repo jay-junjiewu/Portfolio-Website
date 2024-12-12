e2ad3accc5aae1e467d7215938d70b0f0b987d3d
Junjie Wu <z5347759@ad.unsw.edu.au>
2024-12-12 20:44:12 +1100

    Fixed main page path

--- src/scripts/theme-toggle.js
+++ src/scripts/theme-toggle.js
@@ -1,7 +1,7 @@
 // Set default to dark mode on page load based on localStorage
 document.addEventListener('DOMContentLoaded', () => {
     const isDarkMode = localStorage.getItem('theme') === 'dark';
-    const isMainPage = window.location.pathname === '/Portfolio-Website/' || window.location.pathname.endsWith('index.html');
+    const isMainPage = window.location.pathname === '/' || window.location.pathname.endsWith('index.html');
 
 
     if (isDarkMode) {
@@ -60,7 +60,7 @@ const handle = document.querySelector('.ant-switch-handle');
 themeToggle.addEventListener('click', () => {
     const isDarkMode = document.body.classList.toggle('dark-mode');
     const header = document.querySelector('header');
-    const isMainPage = window.location.pathname === '/Portfolio-Website/' || window.location.pathname.endsWith('index.html');
+    const isMainPage = window.location.pathname === '/' || window.location.pathname.endsWith('index.html');
 
 
     const htmlElement = document.documentElement;    
